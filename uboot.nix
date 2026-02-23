812a0a0ffd97d8aa67d826f69e8d6145e29422e5
matthewcroughan <matt@croughan.sh>
2026-02-23 23:47:23 +0000

    maybe using a different device tree will work

--- uboot.nix
+++ uboot.nix
@@ -4,10 +4,13 @@ buildUBoot {
   src = fetchFromGitHub {
     owner = "u-boot";
     repo = "u-boot";
-    rev = "caa740df9cae5ddc74b18b88a4fea43e27b2fdf6";
-    hash = "sha256-+72ZzGtNrS1vOOsBOlujB9gYQ7HZFxywYEBH70QSUD0=";
+    rev = "4f70106beafe30df8b0528a3d8b2543cd48fe241";
+    hash = "sha256-q0XDtxdBqpO3Ih9Oy1dmusp7SBZvEflj/W51Fey16Dc=";
   };
   patches = [];
+  prePatch = ''
+    substituteInPlace configs/liontron-h-a133l_defconfig --replace-fail 'CONFIG_DEFAULT_DEVICE_TREE="allwinner/sun50i-a133-liontron-h-a133l"' 'CONFIG_DEFAULT_DEVICE_TREE="allwinner/sun50i-a100-allwinner-perf1"'
+  '';
   defconfig = "liontron-h-a133l_defconfig";
   BL31 = "${armTrustedFirmwareAllwinner}/bl31.bin";
   filesToInstall = [ "u-boot-sunxi-with-spl.bin" ];
